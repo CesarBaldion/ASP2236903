c84e38068ffad69474176556620c12a6ac649cf9
Cesar <ratedkhan5412@gmail.com>
2021-08-23 19:05:04 -0500

    Crud Compra

---
+++ ASP2236903/Controllers/CompraController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP2236903.Models;
+
+namespace ASP2236903.Controllers
+{
+    public class CompraController : Controller
+    {
+        // GET: Compra
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.compra.ToList());
+            }
+        }
+
+        public static string NombreCliente(int idCliente)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.cliente.Find(idCliente).nombre;
+            }
+        }
+
+        public ActionResult ListarCliente()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.cliente.ToList());
+            }
+        }
+
+
+        public static string NombreUsuario(int idUsuario)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.usuario.Find(idUsuario).nombre;
+            }
+        }
+
+        public ActionResult ListarUsuario()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.usuario.ToList());
+            }
+        }
+
+        public ActionResult Create(compra compra)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.compra.Add(compra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.compra.Find(id));
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                compra compraEdit = db.compra.Where(a => a.id == id).FirstOrDefault();
+                return View(compraEdit);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(compra compraEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var oldProduct = db.compra.Find(compraEdit.id);
+                    oldProduct.fecha = compraEdit.fecha;
+                    oldProduct.total = compraEdit.total;
+                    oldProduct.id_usuario = compraEdit.id_usuario;
+                    oldProduct.id_cliente = compraEdit.id_cliente;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    compra compra = db.compra.Find(id);
+                    db.compra.Remove(compra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+    }
+}
